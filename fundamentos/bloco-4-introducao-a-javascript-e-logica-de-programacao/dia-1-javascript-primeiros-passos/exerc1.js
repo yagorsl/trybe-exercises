aea3c77efa786d420581b1c39a6d4cf05fbedba9
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-03-29 10:02:01 -0300

    Resolve exerc2 e altera console.log do exerc1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exerc1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exerc1.js
@@ -3,8 +3,8 @@
 const a = 20;
 const b = 25;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+console.log("Soma: " + (a + b));
+console.log("Subtração: " + (a - b));
+console.log("Multiplicação: " + (a * b));
+console.log("Divisão: " + (a / b));
+console.log("Módulo: " + (a % b));
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exerc2.js
@@ -0,0 +1,10 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const num1 = 75;
+const num2 = 85;
+
+if (num1 > num2) {
+  console.log(`${num1} é maior que ${num2}`);
+} else {
+  console.log(`${num2} é maior que ${num1}`);
+}
