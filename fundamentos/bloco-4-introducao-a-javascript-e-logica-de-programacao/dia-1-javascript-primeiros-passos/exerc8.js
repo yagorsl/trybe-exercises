69d83d0f3e3acc98e6b88b3f46687f6a059a21f4
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-03-29 12:11:12 -0300

    Resolve exerc8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exerc8.js
@@ -0,0 +1,13 @@
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+
+const num1 = 21;
+const num2 = 31;
+const num3 = 47;
+
+if ((num1 % 2) === 0 || (num2 % 2) === 0 || (num3 % 2) === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+// usa-se o operador % para verificar se o resto da divisão por 2 é 0. Essa é a condição para um número ser par.
