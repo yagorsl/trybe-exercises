55905dda7e4219b0966d6825b840d373e5453c42
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-03-31 13:03:50 -0300

    Resolve exerc7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script5.js
@@ -1,7 +1,7 @@
 // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let higherNumber = 0;
+let higherNumber = numbers[0];
 
 // O valor da variavel higherNumber poderia ser qualquer, desde que fosse menor do que o maior numero do array numbers. Caso fosse atribuído o valor 101, por exemplo, o loop for nunca encontraria um valor maior que 101 no array, pois o maior número é o 100;
 for (let index = 0; index < numbers.length; index += 1) {
