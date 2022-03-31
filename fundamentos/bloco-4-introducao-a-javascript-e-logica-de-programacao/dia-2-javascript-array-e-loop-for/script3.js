2b8166cb4ffd8741a1935735d08bc36b720cd7c9
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-03-31 12:37:16 -0300

    Resolve exerc3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script3.js
@@ -0,0 +1,12 @@
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result = result + numbers[index];
+}
+
+const average = result / numbers.length;
+
+console.log(average);
