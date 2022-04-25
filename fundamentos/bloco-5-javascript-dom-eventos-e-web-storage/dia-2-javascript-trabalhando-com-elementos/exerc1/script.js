bc1e79acabd0552d1e8779d6159fb8b0b6e52422
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-04-25 16:34:33 -0300

    Resolve exercicios 3 a 5

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exerc1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exerc1/script.js
@@ -7,3 +7,18 @@ document.body.appendChild(elementH1);
 const mainTag = document.createElement('main');
 mainTag.className = 'main-content';
 document.body.appendChild(mainTag);
+
+// 3. Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+const sectionTag = document.createElement('section');
+sectionTag.className = 'center-content';
+mainTag.appendChild(sectionTag);
+
+// 4. Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+const paragraphTag = document.createElement('p');
+paragraphTag.innerText = 'Olá, eu me chamo Yago!';
+sectionTag.appendChild(paragraphTag);
+
+// 5. Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+const leftSection = document.createElement('section');
+leftSection.className = 'left-content';
+mainTag.appendChild(leftSection);
