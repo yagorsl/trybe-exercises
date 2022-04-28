ca37b9fd802e1a5d50c5c5af2966ca05e59d0552
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-04-28 14:47:33 -0300

    Resolve exerc7 implementando a função addTask

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exerc1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exerc1/script.js
@@ -138,3 +138,14 @@ function mouseOut() {
   });
 };
 mouseOut();
+
+// 7. Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+function addTask(task) {
+  const myTasks = document.querySelector('.my-tasks');
+  const span = document.createElement('span');
+  myTasks.appendChild(span);
+  span.innerHTML = task;
+};
+
+addTask('cozinhar');
