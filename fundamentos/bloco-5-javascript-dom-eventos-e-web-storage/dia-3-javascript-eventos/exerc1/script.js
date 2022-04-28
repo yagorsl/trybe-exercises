6aa0212667d5fcea2224a145d1d3c2623a7d3c40
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2022-04-28 13:46:03 -0300

    Resolve exerc4 implementando função addFridayBtn

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exerc1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exerc1/script.js
@@ -78,3 +78,21 @@ function displayHolidays() {
   });
 };
 displayHolidays();
+
+// 4. Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+// Adicione a este botão o ID "btn-friday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+
+function addFridayBtn(string) {
+  const btnContainer = document.querySelector('.buttons-container');
+
+  const fridayButton = document.createElement('button');
+  fridayButton.innerHTML = string;
+  fridayButton.id = 'btn-friday'; 
+
+  btnContainer.appendChild(fridayButton)
+};
+addFridayBtn('Sexta-Feira');
+
+// 5. Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
